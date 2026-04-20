60c7dc8574f2edc59576ac172851f6a86c265f13
fernando <flopez@iolabs.se>
2026-04-20 10:26:12 +0200

    added card components, styling, and components for title and flatlists

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,60 @@
 import "@/global.css";
-import { Link } from "expo-router";
 
+import ListHeading from "@/components/ListHeading";
 import { SafeScreen } from "@/components/SafeScreen";
-import { Pressable, Text } from "react-native";
-
+import UpcomingSubscriptionCard from "@/components/UpcomingSubscriptionCard";
+import { HOME_BALANCE, HOME_USER, UPCOMING_SUBSCRIPTIONS } from "@/constants/data";
+import images from "@/constants/images";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
+import { FlatList, Image, Text, View } from "react-native";
 export default function Index() {
   return (
-    <SafeScreen className="p-5">
-      <Text className="text-xl font-bold text-blue-500 font-sans-bold">Welcome to Nativewind!</Text>
+    <SafeScreen className="flex-1 bg-background p-5"> 
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar" />
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View> 
+        <Image source={images.add} className="home-add-icon" />
+      </View>
 
-      <Link href="/onboarding" asChild>
-        <Pressable className="mt-4 rounded bg-blue-500 px-4 py-3">
-          <Text className="text-white font-semibold">Onboarding</Text>
-        </Pressable>
-      </Link>
-      <Link href="/(auth)/sign-up" asChild>
-        <Pressable className="mt-4 rounded bg-blue-500 px-4 py-3">
-          <Text className="text-white font-semibold">SignUp</Text>
-        </Pressable>
-      </Link>
-      <Link href="/(auth)/sign-in" asChild>
-        <Pressable className="mt-4 rounded bg-blue-500 px-4 py-3">
-          <Text className="text-white font-semibold">SignIn</Text>
-        </Pressable>
-      </Link>
-      <Link href="/subscriptions/spotify" asChild>
-        <Pressable className="mt-4 ">
-          <Text className="text-white font-semibold">SignIn</Text>
-        </Pressable>
-      </Link>
-      <Link href={{
-        pathname: "/subscriptions/[id]",
-        params: { id: "cloude" },
-      }} asChild>
-        <Pressable className="mt-4 ">
-          <Text className="text-white font-semibold">SignIn</Text>
-        </Pressable>
-      </Link>
-    
+      <View className="home-balance-card">
+        <Text className="home-balance-label">Welcome back, {HOME_USER.name}</Text>
+
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">{formatCurrency(HOME_USER.amount)}</Text>
+          <Text className="home-balance-date">{dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD/YYYY")}</Text>
+        </View>
+      </View>
+
+      <View>
+        <ListHeading title="Listheading" />
+        <FlatList 
+        data={UPCOMING_SUBSCRIPTIONS}
+        renderItem={({item}) => (
+          <UpcomingSubscriptionCard {...item}/>
+        )}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        ListEmptyComponent={<Text className="list-empty">No upcoming subscriptions</Text>}
+      />
+      </View>
+      <View>
+        <ListHeading title="Listheading" />
+        <FlatList 
+        data={UPCOMING_SUBSCRIPTIONS}
+        renderItem={({item}) => (
+          <UpcomingSubscriptionCard {...item}/>
+        )}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        ListEmptyComponent={<Text className="list-empty">No upcoming subscriptions</Text>}
+
+      />
+      </View>
     </SafeScreen>
   );
 }
--- constants/images.ts
+++ constants/images.ts
@@ -1,7 +1,9 @@
+import add from "@/assets/icons/add.png";
 import avatar from "@/assets/images/avatar.png";
 import SplashPattern from "@/assets/images/splash-pattern.png";
 
 export default {
     SplashPattern,
     avatar,
+    add,
 }
